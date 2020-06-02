e3fd942e442118b7a76be9fbb8c63f63095459bd
Shady Grant <shadygrant@yahoo.com>
2020-06-02 15:37:32 -0500

    initial commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -21,5 +21,23 @@
 --JOIN Genre genre ON song.GenreId = genre.Id
 --WHERE genre.Label='Pop'
 
+--SELECT
+--artist.ArtistName,
+--genre.Label
+--FROM Song song
+--JOIN Artist artist ON song.ArtistId = artist.Id
+--JOIN Genre genre ON song.GenreId = genre.Id
+--WHERE genre.Label = 'Pop' OR genre.Label= 'Rock';
+
+--SELECT
+--album.Title,
+--Song.AlbumId
+--FROM Album album
+--JOIN Song song ON song.AlbumId = album.Id
+--WHERE album.Id IS NULL;
+
+
+
+
 
 
