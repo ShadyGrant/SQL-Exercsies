8878f4030013f8ef870fc9f2c1ae7f1d10697577
Shady Grant <shadygrant@yahoo.com>
2020-06-03 11:07:38 -0500

    initial commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -36,7 +36,17 @@
 --JOIN Song song ON song.AlbumId = album.Id
 --WHERE album.Id IS NULL;
 
+--INSERT INTO Artist (ArtistName, YearEstablished)
+--VALUES ('Tamer Hosny', '2000');
 
+--INSERT INTO Album (Title, ReleaseDate, AlbumLength, [Label], ArtistId, GenreId)
+--VALUES ('Eish Be shoaak', '07/08/2018', '2240', 'EGY', '28', '10');
+
+--INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+--VALUES ('Naseny Leh', '490', '07/07/2018', '10', '28', '23');
+
+--SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.Id;
+--SELECT a.Title, s.Title FROM Song s LEFT JOIN Album a ON s.AlbumId = a.Id;
 
 
 
